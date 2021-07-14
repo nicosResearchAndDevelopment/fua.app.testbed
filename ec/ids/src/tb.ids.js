08feeff4f7cfee5cebfeeba5212c1fc31d9ad533
jlangkau <jlangkau@nicos-rd.com>
2021-07-14 16:04:44 +0200

    work

---
+++ ec/ids/src/tb.ids.js
@@ -0,0 +1,9 @@
+
+exports.name = "Ich bin IDS";
+
+exports.ec = "ids";
+
+exports.fn   = {
+    'getDATfromDAPS': require(`./fn/getDATfromDAPS/getDATfromDAPS.js`)
+}; // exports.assert
+
---
+++ ec/net/src/tb.net.js
@@ -0,0 +1,9 @@
+
+exports.name = "Ich bin das Netz";
+
+exports.ec = "net";
+
+exports.fn   = {
+    'ping': require(`./fn/ping/ping.js`)
+}; // exports.assert
+
