a98f932801c30edd7f3162b068abdb1a5dcbd632
jlangkau <jlangkau@nicos-rd.com>
2021-07-15 19:13:17 +0200

    init

--- ec/ids/src/tb.ids.js
+++ ec/ids/src/tb.ids.js
@@ -1,9 +1,12 @@
 
-exports.name = "Ich bin IDS";
+exports.ec     = "ids";
 
-exports.ec = "ids";
+exports['$ec'] = "ids";
 
-exports.fn   = {
+exports.fn = {
     'getDATfromDAPS': require(`./fn/getDATfromDAPS/getDATfromDAPS.js`)
-}; // exports.assert
+};
 
+exports['getDATfromDAPS'] = exports.fn['getDATfromDAPS'];
+
+// EOF
