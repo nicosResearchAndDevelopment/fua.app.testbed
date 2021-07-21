d417296561d48d363d4f20c2575359dac21d6a77
Simon Petrac <pfoerdie@gmail.com>
2021-07-21 17:35:27 +0200

    added ping method

---
+++ src/code/SubProcess.js
@@ -0,0 +1,81 @@
+const
+    ExtendedASCII = require('./ExtendedASCII.js'),
+    util          = require('@nrd/fua.core.util'),
+    child_process = require('child_process'),
+    EventEmitter  = require('events');
+
+function flattenArgs(args) {
+    if (util.isArray(args)) {
+        return args.map(flattenArgs).flat(1);
+    } else if (util.isObject(args)) {
+        const res = [];
+        for (let [key, value] of Object.entries(args)) {
+            if (!key.startsWith('-')) key = (key.length > 1 ? '--' : '-') + key;
+            if (util.isArray(value)) {
+                for (let entry of value) {
+                    res.push(key);
+                    res.push(entry);
+                }
+            } else {
+                res.push(key);
+                res.push(value);
+            }
+        }
+        return res;
+    } else {
+        return args;
+    }
+} // flattenArgs
+
+/**
+ * @param {string} command
+ * @param {...any} args
+ * @returns {Promise<string>}
+ */
+exports.execute = async function (command, ...args) {
+    const
+        subprocess = child_process.spawn(command, flattenArgs(args)),
+        chunks     = [];
+
+    subprocess.stdout.on('data', chunks.push.bind(chunks));
+
+    await new Promise((resolve, reject) => {
+        subprocess.on('error', reject);
+        subprocess.on('exit', resolve);
+    });
+
+    const
+        buffer = Buffer.concat(chunks),
+        string = ExtendedASCII.byte2str(buffer);
+
+    return string.trim().replace(/\r\n/g, '\n');
+}; // exports.execute
+
+/**
+ * @param {string} command
+ * @param {...any} args
+ * @returns {Promise<EventEmitter<string>>}
+ */
+exports.spawn = async function (command, ...args) {
+    const
+        subprocess = child_process.spawn(command, flattenArgs(args)),
+        emitter    = new EventEmitter();
+
+    emitter.on('close', () => subprocess.kill());
+
+    let lastRow = '';
+    subprocess.stdout.on('data', (chunk) => {
+        const rows = ExtendedASCII.byte2str(chunk).split(/\r?\n/g);
+        rows[0]    = lastRow + rows[0];
+        lastRow    = rows.pop();
+        rows.forEach((dataRow) => emitter.emit('data', dataRow));
+    });
+
+    subprocess.on('error', (err) => emitter.emit('error', err));
+    subprocess.on('exit', (exitCode) => {
+        emitter.emit('data', lastRow);
+        emitter.emit('end', exitCode);
+    });
+
+    return emitter;
+}; // exports.spawn
