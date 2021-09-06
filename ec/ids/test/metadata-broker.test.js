ac98e2fd22184401f4851360f24da352bb3b7652
Simon Petrac <pfoerdie@gmail.com>
2021-09-06 13:39:37 +0200

    implemented basic tests for omejdn-daps and metadata-broker

--- ec/ids/test/metadata-broker.test.js
+++ ec/ids/test/metadata-broker.test.js
@@ -1,7 +1,41 @@
 const
-    {describe, test} = require('mocha'),
-    expect           = require('expect');
+    {describe, test, before} = require('mocha'),
+    expect                   = require('expect'),
+    path                     = require('path'),
+    fs                       = require('fs/promises'),
+    fetch                    = require('node-fetch'),
+    https                    = require('https');
 
-describe('ec/ids/metadata-broker', function() {
+describe('ec/ids/metadata-broker', function () {
+
+    this.timeout(10e3);
+
+    let agent;
+    before('create a https agent with tls that does not reject self signed certificates', async function () {
+        agent = new https.Agent({rejectUnauthorized: false});
+    });
+
+    test('the "metadata-broker" directory should exist', async function () {
+        const stats = await fs.stat(path.join(__dirname, '../metadata-broker'));
+        expect(stats.isDirectory()).toBeTruthy();
+    });
+
+    test('the API should be accessible on port 8443', async function () {
+        await fetch('https://localhost:8443', {agent});
+    });
+
+    test('the self-description should be available at /', async function () {
+        const
+            response = await fetch('https://localhost:8443/', {agent}),
+            about    = await response.json();
+
+        expect(about).toMatchObject({
+            "@context": {
+                "ids": "https://w3id.org/idsa/core/"
+            },
+            "@type":    "ids:Broker",
+            "@id":      "https://localhost/"
+        });
+    });
 
 });
