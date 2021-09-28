aab548e3393cfd031019b7134171a310be090ab5
Simon Petrac <pfoerdie@gmail.com>
2021-09-28 14:30:42 +0200

    changed ping to subprocess module

--- ec/ip/src/fn/ping/ping.test.js
+++ ec/ip/src/fn/ping/ping.test.js
@@ -10,7 +10,7 @@ describe('ec/ip/ping', function () {
     });
 
     test('ping should be async and return a ping result object', async function () {
-        const result = await ping({address: 'google.com'});
+        const result = await ping({host: 'google.com'});
         expect(typeof result).toBe('object');
         console.log(result);
     });
