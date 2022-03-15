9270ff3c305ad435d8e224ed3abdee219952c831
Simon Petrac <pfoerdie@gmail.com>
2022-03-15 15:04:41 +0100

    backed up current src files

---
+++ back/src/code/agent.Testbed.js
@@ -0,0 +1,234 @@
+const
+    path        = require('path'),
+    //
+    util        = require('@nrd/fua.core.util'),
+    //
+    {Self}      = require(path.join(util.FUA_JS_LIB, 'agent.Self/src/agent.Self.js')),
+    // TODO : beta
+    {Time}      = require(path.join(util.FUA_JS_LIB, 'agent.Time/src/agent.Time.js')),
+
+    {System}    = require(path.join(util.FUA_JS_LIB, 'agent.System/src/agent.System.beta.js')),
+    {Device}    = require(path.join(util.FUA_JS_LIB, 'agent.System/src/agent.System.beta.js')),
+    {Testsuite} = require('./agent.Testsuite.js')// REM: as agent
+;
+
+//region error
+
+class ErrorTestbedIdIsMissing extends Error {
+    constructor(message) {
+        super(`${timestamp()}] : fua.agent.Testbed : Testbed :: ${message}`);
+    }
+}
+
+//endregion error
+
+//region fn
+function timestamp() {
+    return (new Date).toISOString();
+}
+
+//function contextHasPrefix(contexts, prefix) {
+//    let result = false;
+//    for (let i = 0; ((!result) && (i < contexts.length)); i++) {
+//        result = ((contexts[i][prefix]) ? true : false)
+//    } // for (i)
+//    return result;
+//}
+
+//endregion fn
+
+function TestbedAgent({
+                          '@context':               context_parent = [],
+                          '@id':                    id,
+                          'prefix':                 prefix = {
+                              'testbed': "tb:",
+                              'tb':      "tb:",
+                              //'testsuite': "ts:",
+                              //'ts':        "ts:",
+                              'system': "sys:",
+                              'sys':    "sys:",
+                              'domain': "dom:",
+                              'dom':    "dom:"
+                          },
+                          'prefix_self':            prefix_self = "",
+                          'prefix_self_model':      prefix_self_model = "",
+                          'prefix_system':          prefix_system = "",
+                          'prefix_system_model':    prefix_system_model = "",
+                          'prefix_domain':          prefix_domain = "",
+                          'prefix_domain_model':    prefix_domain_model = "",
+                          'prefix_ldp_model':       prefix_ldp_model = "",
+                          'prefix_testsuite':       prefix_testsuite = "",
+                          'prefix_testsuite_model': prefix_testsuite_model = "",
+                          'prefix_testbed':         prefix_testbed = "",
+                          'prefix_testbed_model':   prefix_testbed_model = "",
+                          //
+                          'type': type = [],
+                          'fn':   fn = undefined,
+                          'node': node = undefined,
+                          'app':  app,
+                          //
+                          'tb_util': tb_util
+                      }) {
+
+    const
+        context_self = context_parent.concat(TestbedAgent['@context']), // REM: self context
+        owner        = app['owner'] // TODO : sollte auch eine async function sein, wenn die app implementiert ist...
+    ;
+    let
+        tmp_prefix,
+        tmp_node
+    ;
+
+    id = ((id) ? (`${id}agent/`) : (`${tb_util['idAsBlankNode']()}/agent/`));
+    type.push(TestbedAgent);
+
+    fn = (fn || (async function /** as self */ testbedAgent() {
+        try {
+            let presentation = {
+                '@context': undefined,
+                '@id':      testbedAgent['@id'],
+                '@type':    ((testbedAgent['@type']) ? testbedAgent['@type'].map((type) => {
+                    return (type['@id'] || type);
+                }) : undefined)
+            };
+
+            let temp_predicate;
+            temp_predicate = `owner`;
+            if (testbedAgent[temp_predicate])
+                presentation[temp_predicate] = await testbedAgent[temp_predicate]();
+
+            temp_predicate = `scheduler`;
+            if (testbedAgent[temp_predicate] && !presentation[temp_predicate])
+                presentation[temp_predicate] = await testbedAgent[temp_predicate]();
+
+            //temp_predicate = `${prefix_system_model}system`;
+            temp_predicate = `system`;
+            if (testbedAgent[temp_predicate] && !presentation[temp_predicate])
+                presentation[temp_predicate] = await testbedAgent[temp_predicate]();
+
+            //temp_predicate = `${prefix_domain_model}domain`;
+            temp_predicate = `domain`;
+            if (testbedAgent[temp_predicate] && !presentation[temp_predicate])
+                presentation[temp_predicate] = await testbedAgent[temp_predicate]();
+
+            temp_predicate = `${prefix_testbed_model}testsuite`;
+            if (testbedAgent[temp_predicate] && !presentation[temp_predicate])
+                presentation[temp_predicate] = await testbedAgent[temp_predicate]();
+
+            return presentation;
+        } catch (jex) {
+            throw jex;
+        } // try
+    }));
+
+    if (new.target) {
+        if (!node['@id'])
+            throw new ErrorTestbedIdIsMissing(`id is missing on node`)
+        Object.defineProperties(fn, {
+            '@id':   {value: node['@id'], enumerable: true},
+            '@type': {value: type, enumerable: true}
+        });
+    } // if ()
+
+    Self({
+        '@context': context_self,
+        '@id':      id,
+        'type':     type,
+        //
+        'prefix':                 {
+            'self':   prefix.testbed,
+            'system': prefix.sys,
+            'sys':    prefix.sys,
+            'domain': prefix.dom,
+            'dom':    prefix.dom
+        },
+        'prefix_self':            prefix_self,
+        'prefix_self_model':      prefix_self_model,
+        'prefix_system':          prefix_system,
+        'prefix_system_model':    prefix_system_model,
+        'prefix_domain':          prefix_domain,
+        'prefix_domain_model':    prefix_domain_model,
+        'prefix_ldp_model':       prefix_ldp_model,
+        'prefix_testsuite':       prefix_testsuite,
+        'prefix_testsuite_model': prefix_testsuite_model,
+        'prefix_testbed':         prefix_testbed,
+        'prefix_testbed_model':   prefix_testbed_model,
+        //
+        'System':    Device,
+        'domain':    undefined,
+        'scheduler': undefined,
+        //
+        'fn':   fn,
+        'node': node,
+        //
+        'contextHasPrefix': tb_util['contextHasPrefix'],
+        'idAsBlankNode':    tb_util['idAsBlankNode'],
+        'randomLeaveId':    tb_util['randomLeaveId']
+    });
+
+    tmp_node = (node['testsuite'] || node[`${prefix_testbed_model}testsuite`]);
+    if (tmp_node)
+        // REM : object-property 'testsuite' comes from testbed-model ('tbm')
+        Object.defineProperty(fn, `${prefix_testbed_model}testsuite`, {
+            value:          new Testsuite({
+                'prefix_self':            prefix_self,
+                'prefix_self_model':      prefix_self_model,
+                'prefix_system':          prefix_system,
+                'prefix_system_model':    prefix_system_model,
+                'prefix_domain':          prefix_domain,
+                'prefix_domain_model':    prefix_domain_model,
+                'prefix_testsuite':       prefix_testsuite,
+                'prefix_testsuite_model': prefix_testsuite_model,
+                'prefix_testbed':         prefix_testbed,
+                'prefix_testbed_model':   prefix_testbed_model,
+                //
+                'type': [],
+                //
+                'self':   false, // REM : to prevent testsuite to get self once again...
+                'system': fn[`${prefix_system_model}system`],
+                'domain': fn[`${prefix_domain_model}domain`],
+                //
+                'fn':   undefined,
+                'node': tmp_node,
+                //
+                'contextHasPrefix': tb_util['contextHasPrefix'],
+                'idAsBlankNode':    tb_util['idAsBlankNode']
+            }), enumerable: true
+        });
+
+    fn['on'](fn['on_task']['scheduler_idle'], (data) => {
+        //debugger;
+        console.log(`fn['on'](fn['on_task']['scheduler_idle'] : data <${JSON.stringify(data)}>`);
+        return undefined;
+    });
+    fn['on'](fn['on_task']['scheduler_error'], (error) => {
+        //debugger;
+        console.log(`fn['on'](fn['on_task']['scheduler_error'] : data <${JSON.stringify(data)}>`);
+        return undefined;
+    });
+    Object.freeze(fn);
+
+    return fn;
+} // TestbedAgent
+
+Object.defineProperties(TestbedAgent, {
+    '@context': {
+        value:          [{
+            "@base": "https://testbed.nicos-rd.com",
+            "vocab": "/",
+            "tb":    "https://testbed.nicos-rd.com/",
+            "tbm":   "https://testbed.nicos-rd.com/",
+            //
+            "sysm": "https://www.nicos-rd.com/fua/agent/system#",
+            "domm": "https://www.nicos-rd.com/fua/agent/domain#",
+            //
+            "system": "https://www.nicos-rd.com/fua/agent/system#system",
+            "domain": "https://www.nicos-rd.com/fua/agent/domain#domain",
+            // TODO : ha to be switched, when testsuite has its own app...
+            "testsuite": "https://testsuite.nicos-rd.com/"
+        }], enumerable: true
+    },
+    '@id':      {value: "https://www.nicos-rd.com/fua/testbed#TestbedAgent/"}
+});
+
+exports.TestbedAgent = TestbedAgent;
---
+++ back/src/code/browse/ldp.method.mjs
@@ -0,0 +1,100 @@
+import {assert, is} from '/browse/lib/web.lib.core.mjs';
+import * as model from './ldp.model.mjs';
+
+const
+    _context           = {
+        'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
+        'ldp':  'http://www.w3.org/ns/ldp#'
+    },
+    _baseURL           = (({protocol, host}) => protocol + '//' + host + '/')(new URL(document.baseURI)),
+    _baseURI           = (({hostname}) => 'http://' + hostname + '/')(new URL(document.baseURI)),
+    _StringValidator   = (re) => (str) => is.string(str) && re.test(str),
+    _isApplicationJSON = _StringValidator(/application\/(?:ld\+)?json/);
+
+export function resolveURL(url) {
+    const [prefix] = /^[a-z][a-z0-9+\-._]*(?=:)/i.exec(url) ?? [];
+    if (prefix && _context[prefix]) url = _context[prefix] + url.substr(prefix.length + 1);
+    if (url.startsWith(_baseURI)) url = url.substr(_baseURI.length);
+    return new URL(url, _baseURL).toString();
+}
+
+export function resolveURI(url) {
+    if (url.startsWith(_baseURL)) url = url.substr(_baseURL.length);
+    return new URL(url, _baseURI).toString();
+}
+
+export async function GET(uri) {
+    if (uri instanceof model.BaseNode) uri = uri.id;
+    else assert(is.string(uri), 'expected uri to be a string', TypeError);
+
+    const
+        targetURL = resolveURL(uri),
+        targetURI = resolveURI(uri),
+        response  = await fetch(targetURL, {
+            method:  'GET',
+            cache:   'no-store',
+            headers: {
+                'Accept': 'application/ld+json'
+            }
+        });
+
+    assert(response.ok, `${response.url} [${response.status}]: ${response.statusText}`);
+    assert(response.headers.has('ETag'), 'expected headers to contain ETag');
+    assert(_isApplicationJSON(response.headers.get('Content-Type')),
+        'expected headers to contain Content-Type of application/[ld+]json');
+
+    const {'@graph': graph, '@context': context} = await response.json();
+    assert(is.array(graph), 'expected response to include an @graph array ', TypeError);
+    if (context) {
+        assert(is.object(context) && Object.values(context).every(iri => is.string(iri)),
+            'expected @context to be an object of strings', TypeError);
+        Object.assign(_context, context);
+    }
+
+    const nodes = new Map();
+    for (let node of graph) {
+        assert(is.object(node) && is.string(node['@id']), 'expected @graph to only contain json-ld nodes', TypeError);
+        assert(!nodes.has(node['@id']), 'expected @graph to not contain duplicate nodes');
+        nodes.set(node['@id'], node);
+    }
+
+    assert(nodes.has(targetURI), 'expected @graph to contain the requested uri');
+    const targetNode = (function _nonCircularMeshing(node, blacklist) {
+        for (let [key, prop] of Object.entries(node)) {
+            if (!key.startsWith('@') && Array.isArray(prop)) {
+                node[key] = prop.map(function (child) {
+                    if (!(child['@id'] && nodes.has(child['@id']))) return child;
+                    if (blacklist.includes(child['@id'])) return child;
+                    return _nonCircularMeshing(nodes.get(child['@id']), [...blacklist, child['@id']]);
+                });
+            }
+        }
+        return node;
+    })(nodes.get(targetURI), [targetURI]);
+
+    return model.construct(targetNode, response.headers.get('ETag'));
+} // function GET
+
+export async function HEAD() {
+    // TODO
+} // function HEAD
+
+export async function OPTION() {
+    // TODO
+} // function OPTION
+
+export async function POST() {
+    // TODO
+} // function POST
+
+export async function PUT() {
+    // TODO
+} // function PUT
+
+export async function PATCH() {
+    // TODO
+} // function PATCH
+
+export async function DELETE() {
+    // TODO
+} // function DELETE
