b7cd2993bf5b4dc7b3965305be58c8e0c0f0e36e
Simon Petrac <pfoerdie@gmail.com>
2022-06-28 12:44:28 +0200

    added new pattern to EcosystemLDP

--- ec/ldp/src/initialize.ldp.js
+++ ec/ldp/src/initialize.ldp.js
@@ -1,8 +1,7 @@
 const
-    path         = require('path'),
     util         = require('../../../src/code/util.testbed.js'),
     TestbedAgent = require('../../../src/code/agent.testbed.js'),
-    EC_ldp       = require('./tb.ec.ldp.js'),
+    EcosystemLDP = require('./tb.ec.ldp.js'),
     EC_NAME      = 'ldp';
 
 module.exports = async function initializeLDP(
@@ -13,18 +12,13 @@ module.exports = async function initializeLDP(
     util.assert(agent instanceof TestbedAgent, 'expected agent to be a TestbedAgent');
     util.assert(!agent.ecosystems[EC_NAME], 'expected ecosystem ' + EC_NAME + ' not to be initialized already');
 
-    agent.ecosystems[EC_NAME] = 'initializing';
-
-    const ec_ldp = EC_ldp.ldp;
+    const ec_ldp = agent.ecosystems[EC_NAME] = new EcosystemLDP();
+    util.lockProp(agent.ecosystems, EC_NAME);
 
     ec_ldp.on('error', (error) => {
         util.logError(error);
-        // agent.emit('error', error);
         debugger;
     });
 
-    agent.ecosystems[EC_NAME] = Object.freeze(ec_ldp);
-    util.lockProp(agent.ecosystems, EC_NAME);
-
     return ec_ldp;
-}; // module.exports = async function initializeIDS
+}; // module.exports = async function initializeLDP
--- ec/ldp/src/tb.ec.ldp.js
+++ ec/ldp/src/tb.ec.ldp.js
@@ -1,12 +1,31 @@
 const
     EventEmitter = require('events'),
-    //
-    sendRequest  = require(`./fn/sendRequest/sendRequest.js`),
-    //
-    ec_ldp       = new EventEmitter();
+    util         = require('../../../src/code/util.testbed.js'),
+    _sendRequest = require('./fn/sendRequest/sendRequest.js');
 
-Object.defineProperties(ec_ldp, {
-    sendRequest: {value: sendRequest, enumerable: false}
-});
+class EcosystemLDP {
 
-exports.ldp = ec_ldp;
+    #emitter = new EventEmitter();
+
+    on(eventName, listener) {
+        this.#emitter.on(eventName, listener);
+        return this;
+    } // EcosystemLDP#on
+
+    once(eventName, listener) {
+        this.#emitter.once(eventName, listener);
+        return this;
+    } // EcosystemLDP#once
+
+    off(eventName, listener) {
+        this.#emitter.off(eventName, listener);
+        return this;
+    } // EcosystemLDP#off
+
+    async sendRequest(param) {
+        return await _sendRequest(param);
+    } // EcosystemLDP#sendRequest
+
+} // EcosystemLDP
+
+module.exports = EcosystemLDP;
