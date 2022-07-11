e8f33f14527c4c90ac2969d8f7c615bc84ee77b5
Simon Petrac <pfoerdie@gmail.com>
2022-07-11 15:28:25 +0200

    wip: improved script and added questionnaire.ttl

--- testsuite/src/script/ts.render.ids3c-co.js
+++ testsuite/src/script/ts.render.ids3c-co.js
@@ -1,59 +1,57 @@
 const
-    path           = require('path'),
-    expect         = require('expect'),
-    __root         = path.join(__dirname, '../..'),
-    util           = require('../code/util.testsuite.js'),
-    TestsuiteAgent = require('../code/agent.testsuite.js'),
-    context        = require('@nrd/fua.resource.data/context'),
-    rdf            = require('@nrd/fua.module.rdf'),
-    {Dataset}      = require('@nrd/fua.module.persistence');
+    path                   = require('path'),
+    fs                     = require('fs/promises'),
+    expect                 = require('expect'),
+    __data                 = path.join(__dirname, '../../data'),
+    util                   = require('../code/util.testsuite.js'),
+    context                = require('@nrd/fua.resource.data/context'),
+    rdf                    = require('@nrd/fua.module.rdf'),
+    {Dataset, DataFactory} = require('@nrd/fua.module.persistence'),
+    FilesystemStore        = require('@nrd/fua.module.persistence.filesystem'),
+    {Space}                = require('@nrd/fua.module.space'),
+    REPLACE                = false;
 
 (async function Main() {
 
-    const agent = await TestsuiteAgent.create({
-        schema:   'https',
-        hostname: 'testsuite.nicos-rd.com',
-        port:     8081,
-        context:  {
+    const space = new Space({
+        store: new FilesystemStore({
+            defaultFile: 'file://ts.data.ttl',
+            loadFiles:   [
+                {
+                    '@id':             'file://ts.data.ttl',
+                    'dct:identifier':  path.join(__data, 'ts.data.ttl'),
+                    'dct:format':      'text/turtle',
+                    'dct:title':       'data.ttl',
+                    'dct:alternative': 'Testsuite Data'
+                },
+                {
+                    'dct:identifier':  path.join(__data, 'questionnaire/models/model.form.ttl'),
+                    'dct:format':      'text/turtle',
+                    'dct:title':       'model.form.ttl',
+                    'dct:alternative': 'Form Ontology'
+                },
+                require('@nrd/fua.resource.ontology.ids/ids3cm'),
+                require('@nrd/fua.resource.ontology.ids/ids3c-co')
+            ]
+        }, new DataFactory({
             ...context,
             'ids3cm':   'https://w3id.org/ids3cm/',
             'ids3c-co': 'https://w3id.org/ids3c-component/'
-        },
-        store:    {
-            module:  '@nrd/fua.module.persistence.filesystem',
-            options: {
-                defaultFile: 'file://ts.data.ttl',
-                loadFiles:   [
-                    {
-                        '@id':             'file://ts.data.ttl',
-                        'dct:identifier':  path.join(__root, 'data/ts.data.ttl'),
-                        'dct:format':      'text/turtle',
-                        'dct:title':       'data.ttl',
-                        'dct:alternative': 'Testsuite Data'
-                    },
-                    {
-                        'dct:identifier':  path.join(__root, 'data/questionnaire/models/model.form.ttl'),
-                        'dct:format':      'text/turtle',
-                        'dct:title':       'model.form.ttl',
-                        'dct:alternative': 'Form Ontology'
-                    },
-                    require('@nrd/fua.resource.ontology.ids/ids3cm'),
-                    require('@nrd/fua.resource.ontology.ids/ids3c-co')
-                ]
-            }
-        },
-        prefix:   'ts',
-        testbed:  {
-            schema: 'https',
-            host:   'testbed.nicos-rd.com',
-            port:   8080
-        }
+        }))
     });
 
-    expect(agent.space).toBeTruthy();
-
-    const ids_questionnaire = await loadQuestionnaire(agent);
+    const ids_questionnaire = await loadQuestionnaire(space);
     expect(ids_questionnaire.size).toBeGreaterThan(0);
+
+    try {
+        const
+            ttlContent   = await rdf.serializeDataset(ids_questionnaire, 'text/turtle'),
+            filePath     = path.join(__data, 'questionnaire/ids3c-co.questionnaire.ttl'),
+            writeOptions = REPLACE ? {flag: 'w'} : {flag: 'wx'};
+        await fs.writeFile(filePath, ttlContent, writeOptions);
+    } catch (err) {
+        if (err.code !== 'EEXIST') throw err;
+    }
 
     const nrd_questionnaire = new Dataset(null, ids_questionnaire.factory);
 
@@ -62,9 +60,9 @@ const
 
 })().catch(util.logError);
 
-async function loadQuestionnaire(agent) {
+async function loadQuestionnaire(space) {
     const
-        questionnaire = await agent.space.getNode('ids3c-co:IDS_CheckListApproach_Questionnaire').load(),
+        questionnaire = await space.getNode('ids3c-co:IDS_CheckListApproach_Questionnaire').load(),
         dataset       = questionnaire.dataset();
 
     for (let criteriaGroup of questionnaire.getNodes('ids3cm:criteriaGroup')) {
