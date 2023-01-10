80573068a54c25d8ddf79ff50b29f0a4c8d6a135
Simon Petrac <pfoerdie@gmail.com>
2023-01-10 12:36:08 +0100

    removed backup code from apps -> can be viewed in git history

--- back/src/code/browse/ext/socket.io/wrapper.io.mjs
+++
@@ -1,9 +0,0 @@
-if (!('io' in window)) {
-	const ioScript = document.createElement("script");
-	ioScript.setAttribute("src", "/socket.io/socket.io.js");
-	document.head.appendChild(ioScript);
-}
-
-export default function (namespace, options) {
-	return io(namespace, options);
-};
--- back/src/code/browse/ext/svg.js/bundle.svg-2.7.1.mjs
+++
@@ -1,7 +0,0 @@
-/** {@link https://svgjs.com/docs/2.7/ SVG.js} */
-
-// import "./dist/svg.js";
-import "./dist/svg.min.js";
-import "./dist/svg.panzoom.js";
-
-export default SVG;
