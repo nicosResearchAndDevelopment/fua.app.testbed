b8c0f4dd6933ccab400a4987921b0358739608de
Simon Petrac <pfoerdie@gmail.com>
2023-06-05 16:13:56 +0200

    improved ping for windows and linux usage

--- ec/net/src/tm/tb.ec.net.tm.ping.js
+++ ec/net/src/tm/tb.ec.net.tm.ping.js
@@ -2,22 +2,33 @@ const
     util               = require('../tb.ec.net.util.js'),
     testing            = require('@nrd/fua.module.testing'),
     {ExecutionProcess} = require('@nrd/fua.module.subprocess'),
-    cmd_ping           = ExecutionProcess('ping', {encoding: 'cp437'});
+    OS_WIN32           = util.OS_PLATFORM === 'win32',
+    cmd_ping           = ExecutionProcess('ping', OS_WIN32 ? {encoding: 'cp437'} : {});
 
 /** @type {fua.module.testing.TestMethod} */
 module.exports = new testing.Method({
     '@id': 'urn:tb:ec:net:tm:ping',
     /** @param {fua.module.testing.TestToken} token */
     async executor(token) {
-        const
-            output = await cmd_ping({'n': 1}, token.param.host),
-            parts  = output.split(/\r?\n\r?\n/);
-
-        return {
-            timestamp: util.utcDateTime(),
-            info:      parts[0],
-            statistic: parts[1] || null,
-            isAlive:   !!parts[1]
-        };
+        try {
+            const output           = await cmd_ping(OS_WIN32 ? {'n': 1} : {'c': 1}, token.param.host);
+            const [info, statistic = null] = output.split(/\r?\n\r?\n/);
+            return {
+                timestamp:    util.utcDateTime(),
+                info, statistic,
+                isAlive:      !!statistic,
+                pingSend:     true,
+                pingReceived: !!statistic
+            };
+        } catch (err) {
+            const [info, statistic = null] = err.message.split(/\r?\n\r?\n/);
+            return {
+                timestamp:    util.utcDateTime(),
+                info, statistic,
+                isAlive:      false,
+                pingSend:     !!statistic,
+                pingReceived: false
+            };
+        }
     }
 });
