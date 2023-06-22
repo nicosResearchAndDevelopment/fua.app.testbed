37ea5a497dc273a8f07521ee14a2567b5bbf2393
Simon Petrac <pfoerdie@gmail.com>
2023-06-22 12:03:40 +0200

    implemented portscan

--- ec/net/src/tm/tb.ec.net.tm.portscan.js
+++ ec/net/src/tm/tb.ec.net.tm.portscan.js
@@ -2,7 +2,8 @@ const
     util               = require('../tb.ec.net.util.js'),
     testing            = require('@nrd/fua.module.testing'),
     {ExecutionProcess} = require('@nrd/fua.module.subprocess'),
-    cmd_nmap           = ExecutionProcess('nmap', {encoding: 'cp437'});
+    OS_WIN32           = util.OS_PLATFORM === 'win32',
+    cmd_nmap           = ExecutionProcess('nmap', OS_WIN32 ? {encoding: 'cp437'} : {});
 
 /** @type {fua.module.testing.TestMethod} */
 module.exports = new testing.Method({
@@ -10,14 +11,16 @@ module.exports = new testing.Method({
     /** @param {fua.module.testing.TestToken} token */
     async executor(token) {
         const
-            output = await cmd_nmap({'n': 1}, token.param.host),
-            parts  = output.split(/\r?\n\r?\n/);
-
-        util.assertTodo();
+            output       = await cmd_nmap(token.param.host),
+            [tableMatch] = output.replace(/\r/g, '').match(/PORT.*?(?=\n\n|$)/s) || [],
+            tableRows    = tableMatch.split('\n'),
+            tableHeader  = tableRows.shift(),
+            tableFields  = tableHeader.split(/\s+/g).map(value => value.toLowerCase()),
+            tableEntries = tableRows.map(row => Object.fromEntries(row.split(/\s+/g).map((value, index) => [tableFields[index], value])));
 
         return {
-            timestamp: util.utcDateTime()
-            // TODO
+            timestamp: util.utcDateTime(),
+            entries:   tableEntries
         };
     }
 });
