c9013f223635083deef8264949bd13b04887f39c
Simon Petrac <pfoerdie@gmail.com>
2024-01-17 14:14:59 +0100

    added tests for the medadata broker

--- test/tb.test.js
+++ test/tb.test.js
@@ -1,11 +1,80 @@
 const
     {describe, test, before} = require('mocha'),
-    expect                   = require('expect');
+    expect                   = require('expect'),
+    errors                   = require('@nrd/fua.core.errors'),
+    DAPSClient               = require('@nrd/fua.ids.client.daps'),
+    alice                    = require('./alice/index.js');
 
 describe('fua.app.testbed', function () {
 
     test('develop', async function () {
         // TODO
+    });
+
+    describe('metadata-broker', function () {
+
+        this.timeout('10s');
+
+        const dapsUrl   = 'https://daps.tb.nicos-rd.com/';
+        let dapsClient  = null;
+        let aliceDat    = '';
+        const brokerUrl = 'https://mdb.tb.nicos-rd.com/';
+
+        before('request a valid dat', async function () {
+            dapsClient = new DAPSClient({
+                dapsUrl:    dapsUrl,
+                SKIAKI:     alice.client.meta.SKIAKI,
+                privateKey: alice.client.privateKey
+            });
+            aliceDat   = await dapsClient.getDat();
+            await dapsClient.validateDat(aliceDat);
+        });
+
+        test('GET / for the self-description', async function () {
+            const response = await fetch(new URL('/', brokerUrl), {
+                method: 'GET'
+            });
+            if (!response.ok) throw new errors.http.ResponseError(response);
+            const data = await response.json();
+            console.log(data);
+        });
+
+        test('GET /connectors/', async function () {
+            const response = await fetch(new URL('/connectors/', brokerUrl), {
+                method:  'GET',
+                headers: {
+                    'Authorization': `Bearer ${aliceDat}`
+                }
+            });
+            if (!response.ok) throw new errors.http.ResponseError(response);
+            const data = await response.json();
+            console.log(data);
+        });
+
+        test('GET /catalog/', async function () {
+            const response = await fetch(new URL('/catalog/', brokerUrl), {
+                method:  'GET',
+                headers: {
+                    'Authorization': `Bearer ${aliceDat}`
+                }
+            });
+            if (!response.ok) throw new errors.http.ResponseError(response);
+            const data = await response.json();
+            console.log(data);
+        });
+
+        test('POST /infrastructure/', async function () {
+            const response = await fetch(new URL('/infrastructure/', brokerUrl), {
+                method:  'POST',
+                headers: {
+                    'Authorization': `Bearer ${aliceDat}`
+                }
+            });
+            if (!response.ok) throw new errors.http.ResponseError(response);
+            const data = await response.json();
+            console.log(data);
+        });
+
     });
 
 });
